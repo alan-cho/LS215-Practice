01b260d422362f190f0e348df6aee6b4e00565fa
Alan Cho <alancho397@gmail.com>
2023-09-03 21:10:08 -0500

    Added solution four

--- list.js
+++ list.js
@@ -77,3 +77,19 @@ multiplyAllPairs([2, 4], [4, 3, 1, 2]); // [2, 4, 4, 6, 8, 8, 12, 16]
 */
 
 // 4)
+/*
+function sumOfSums(numbers) {
+  return numbers
+    .map((number, idx) =>
+      numbers.slice(0, idx + 1).reduce((sum, value) => sum + value)
+    )
+    .reduce((sum, value) => sum + value);
+}
+
+sumOfSums([3, 5, 2]); // (3) + (3 + 5) + (3 + 5 + 2) --> 21
+sumOfSums([1, 5, 7, 3]); // (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) --> 36
+sumOfSums([4]); // 4
+sumOfSums([1, 2, 3, 4, 5]); // 35
+*/
+
+// 5)
