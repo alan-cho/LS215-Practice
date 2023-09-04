b6851e114ffbc1cae3a2e24b4e20457167a56b04
Alan Cho <alancho397@gmail.com>
2023-09-04 17:43:51 -0500

    added solution ten

--- list.js
+++ list.js
@@ -192,3 +192,65 @@ buyFruit([
 */
 
 // 9)
+/*
+function transactionsFor(id, transactions) {
+  let parsedTransactions = transactions.filter(
+    (transaction) => transaction.id === id
+  );
+  return parsedTransactions;
+}
+
+const transactions = [
+  { id: 101, movement: "in", quantity: 5 },
+  { id: 105, movement: "in", quantity: 10 },
+  { id: 102, movement: "out", quantity: 17 },
+  { id: 101, movement: "in", quantity: 12 },
+  { id: 103, movement: "out", quantity: 15 },
+  { id: 102, movement: "out", quantity: 15 },
+  { id: 105, movement: "in", quantity: 25 },
+  { id: 101, movement: "out", quantity: 18 },
+  { id: 102, movement: "in", quantity: 22 },
+  { id: 103, movement: "out", quantity: 15 },
+];
+
+console.log(transactionsFor(101, transactions));
+*/
+
+// 10)
+/*
+function isItemAvailable(id, transactions) {
+  let transactionsForItem = transactions.filter(
+    (transaction) => transaction.id === id
+  );
+  let amount = 0;
+  transactionsForItem.forEach((transaction) => {
+    if (transaction.movement === "in") {
+      amount += transaction.quantity;
+    } else {
+      amount -= transaction.quantity;
+    }
+  });
+
+  if (amount > 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const transactions = [
+  { id: 101, movement: "in", quantity: 5 },
+  { id: 105, movement: "in", quantity: 10 },
+  { id: 102, movement: "out", quantity: 17 },
+  { id: 101, movement: "in", quantity: 12 },
+  { id: 103, movement: "out", quantity: 15 },
+  { id: 102, movement: "out", quantity: 15 },
+  { id: 105, movement: "in", quantity: 25 },
+  { id: 101, movement: "out", quantity: 18 },
+  { id: 102, movement: "in", quantity: 22 },
+  { id: 103, movement: "out", quantity: 15 },
+];
+
+console.log(isItemAvailable(101, transactions)); // false
+console.log(isItemAvailable(105, transactions)); // true
+*/
