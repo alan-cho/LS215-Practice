4c3c2a01f5df6f09499a053a0cab4bb1a416ca36
Alan Cho <alancho397@gmail.com>
2023-09-03 21:08:14 -0500

    Added solution three

--- list.js
+++ list.js
@@ -60,3 +60,20 @@ alphabeticNumberSort([
 */
 
 // 3)
+/*
+function multiplyAllPairs(array1, array2) {
+  const result = [];
+
+  array1.forEach((number1) => {
+    array2.forEach((number2) => {
+      result.push(number1 * number2);
+    });
+  });
+
+  return result.sort((a, b) => a - b);
+}
+
+multiplyAllPairs([2, 4], [4, 3, 1, 2]); // [2, 4, 4, 6, 8, 8, 12, 16]
+*/
+
+// 4)
