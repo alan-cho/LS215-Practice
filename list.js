3a2ff6787115ee1cb73cf0510c533d796fef6394
Alan Cho <alancho397@gmail.com>
2023-09-04 17:33:51 -0500

    added solution eight

--- list.js
+++ list.js
@@ -167,3 +167,28 @@ palindromes("hello-madam-did-madam-goodbye");
 */
 
 // 8)
+/*
+function buyFruit(fruitsList) {
+  return fruitsList
+    .map((fruit) => repeat(fruit))
+    .reduce((groceryList, fruit) => groceryList.concat(fruit));
+}
+
+function repeat(fruit) {
+  const result = [];
+
+  for (let i = 0; i < fruit[1]; i += 1) {
+    result.push(fruit[0]);
+  }
+
+  return result;
+}
+
+buyFruit([
+  ["apple", 3],
+  ["orange", 1],
+  ["banana", 2],
+]);
+*/
+
+// 9)
