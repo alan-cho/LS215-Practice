8077575cebdae36b8b6df8f5161db216a32fc673
Alan Cho <alancho397@gmail.com>
2023-09-05 21:50:42 -0500

    added solution one

---
+++ string.js
@@ -0,0 +1,15 @@
+// 1)
+/*
+function isUppercase(string) {
+  return !/[a-z]/.test(string);
+}
+
+isUppercase("t"); // false
+isUppercase("T"); // true
+isUppercase("Four Score"); // false
+isUppercase("FOUR SCORE"); // true
+isUppercase("4SCORE!"); // true
+isUppercase(""); // true
+*/
+
+// 2)
