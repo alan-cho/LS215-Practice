d7ebbc241e838713d1eb6d402b3121d97c15ad82
Alan Cho <alancho397@gmail.com>
2023-09-07 18:08:47 -0500

    solved easy problems

---
+++ Easy/easyOne.js
@@ -0,0 +1,50 @@
+// 1)
+/*
+function logOdd() {
+  let oddNum = 1;
+  while (oddNum <= 99) {
+    console.log(oddNum);
+    oddNum += 2;
+  }
+}
+
+logOdd();
+*/
+
+// 2)
+/*
+function logEven() {
+  let evenNum = 2;
+  while (evenNum <= 100) {
+    console.log(evenNum);
+    evenNum += 2;
+  }
+}
+
+logEven();
+*/
+
+// 3)
+/*
+function calculateSqMeters(width, length) {
+  let squareMeters = width * length;
+  let squareFeet = squareMeters * 10.7639;
+  return squareMeters, squareFeet;
+}
+*/
+
+// 4)
+/*
+function calculateTip(bill, percentage) {
+  return bill + bill * (percentage / 100);
+}
+*/
+
+// 6)
+/*
+function short(sOne, sTwo) {
+  return sOne + sTwo + sOne;
+}
+*/
+
+// 7)
