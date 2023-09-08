8dd6475fcefdbae763791b64037dfabc8f660ffd
Alan Cho <alancho397@gmail.com>
2023-09-08 02:28:44 -0500

    added solution one

---
+++ Medium/mediumOne.js
@@ -0,0 +1,17 @@
+// 1)
+/*
+function rotateArray(array) {
+  if (!Array.isArray(array)) {
+    return undefined;
+  }
+  let copy = array.slice();
+  let first = copy.shift();
+  copy.push(first);
+
+  return copy;
+}
+
+console.log(rotateArray(["a", "b", "c"]));
+*/
+
+// 2)
